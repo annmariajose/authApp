3528201e30e7606e4d74ff0a09ebcb8755db0219
Ann Maria Jose <annmariajose1412@gmail.com>
2024-04-12 18:50:22 -0500

    Comment users

--- index.js
+++ index.js
@@ -98,6 +98,6 @@ app.get('/logout',
 
 /* REGISTER SOME USERS */
 
-UserDetails.register({username: 'paul', active: false}, 'paul');
-UserDetails.register({username: 'joy', active: false}, 'joy');
-UserDetails.register({username: 'ray', active: false}, 'ray');
+// UserDetails.register({username: 'paul', active: false}, 'paul');
+// UserDetails.register({username: 'joy', active: false}, 'joy');
+// UserDetails.register({username: 'ray', active: false}, 'ray');
